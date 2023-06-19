e17b3019876dd7cc32ffc32ddd62a89ef80ff6fa
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-06-19 13:13:20 +0600

    all set up ready for intern second project dashboard

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Pages/Layout/Main.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
